2b3d1119d468bb87c9de1039a21f84699dfb6f80
negorokosei <n1910510@gl.cc.uec.ac.jp>
2024-04-26 12:37:36 +0900

    ex2 complete!

--- ex2.f90
+++ ex2.f90
@@ -1,38 +1,17 @@
-! function add_sub(x) !四則演算を指定する関数
-!    implicit none
-!    real(8),intent(in):: x !xが読み取るだけの引数であることを明示
-!    real(8) add_sub !関数(の戻り値)がreal(8)型に指定
-!    add_sub=x-0.01d0
-! end function add_sub
-! program ex2
-!    implicit none
-!    real(8) x
-!    real(8) add_sub !関数をreal(8)型に指定(プログラム側でも指定が必要)
-!    integer i,n,is,iost
-!    open (10,file='ex2_input.d',action='read',iostat=is)
-!    if(is/=0) stop 'cannot open input file'    !読み取り用ファイルの確認
-!    open (11,file='ex2_output.d',action='write',iostat=is)
-!    if(is/=0) stop 'cannot open output file'    !書き込み用ファイルの確認
-!    do i=1,100
-!       read(10,*,iostat=iost)n,x  !ファイルの終了条件が検出されたとき負になる。
-!       if(iost < 0) exit
-!       x=add_sub(x)
-!       write(11,'(i3,a,f10.3)')n,' ',x
-!    enddo
-!    close(10)
-!    close(11)
-! end program ex2
-
-! function add_sub(x) !四則演算を指定する関数(配列ver)
-!    implicit none
-!    real(8),intent(in):: x !xが読み取るだけの引数であることを明示
-!    real(8) add_sub !関数(の戻り値)がreal(8)型に指定
-!    add_sub=x-0.01d0
-! end function add_sub
+module add_sub
+   implicit none
+contains
+   subroutine ab(x)
+      real(8),intent(inout)::x(:) !形状引継ぎ配列
+      integer i
+      i=size(x,1)
+      x(1:i)=x(1:i)+0.2
+   end subroutine ab
+end module add_sub
 program ex2
+   use add_sub
    implicit none
    real(8) , allocatable::x(:)
-   real(8) add_sub !関数をreal(8)型に指定(プログラム側でも指定が必要)
    integer ,allocatable ::n(:)
    integer i,is,iost,nn
    nn=100
@@ -47,7 +26,7 @@ program ex2
    nn=i-1
    close(10)
 
-   x(:)=x(:)+0.2 !任意の四則演算
+   call ab(x) !任意の四則演算
 
    open (11,file='ex2_output.d',action='write',iostat=is)
    if(is/=0) stop 'cannot open output file'    !書き込み用ファイルの確認
---
+++ ex2_01.f90
@@ -0,0 +1,40 @@
+module func
+   implicit none
+contains
+   function add_sub(x) result (a)!四則演算を指定する関数
+      real(8),intent(in):: x !xが読み取るだけの引数であることを明示
+      real(8) a
+      a=x-0.03d0
+   end function add_sub
+end module func
+
+!subroutineを用いた解法
+! module sub
+!    implicit none
+! contains
+   ! subroutine add_sub(x)
+   !    real(8),intent(inout):: x !xが読み取るだけの引数であることを明示
+   !    x=x-0.05d0
+   ! end subroutine add_sub
+! end module sub
+
+program ex2
+   use func
+   !use sub
+   implicit none
+   real(8) x
+   integer i,n,is,iost
+   open (10,file='ex2_input.d',action='read',iostat=is)
+   if(is/=0) stop 'cannot open input file'    !読み取り用ファイルの確認
+   open (11,file='ex2_output.d',action='write',iostat=is)
+   if(is/=0) stop 'cannot open output file'    !書き込み用ファイルの確認
+   do i=1,100
+      read(10,*,iostat=iost)n,x  !ファイルの終了条件が検出されたとき負になる。
+      if(iost < 0) exit
+      !call add_sub(x)
+      x=add_sub(x)
+      write(11,'(i3,a,f10.3)')n,' ',x
+   enddo
+   close(10)
+   close(11)
+end program ex2
